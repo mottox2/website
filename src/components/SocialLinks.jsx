dc961b469033f652cfbcf947fc1a3345ceff17a8
mottox2 <y.mottox2@gmail.com>
2018-05-21 18:32:00 +0900

    Add hatena button

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -27,10 +27,43 @@ class SocialLinks extends Component {
             {count => <div className="share-count">{filter(count)}</div>}
           </FacebookShareCount> */}
         </FacebookShareButton>
+        <div
+          role="button"
+          tabIndex="0"
+          className="SocialMediaShareButton"
+          onClick={() => {
+            window.open(`http://b.hatena.ne.jp/entry/${url}`)
+          }}
+        >
+          <div style={{ width: 42, height: 42, position: 'relative' }}>
+            <svg
+              viewBox="0 0 64 64"
+              width="42"
+              height="42"
+              className="social-icon social-icon--facebook "
+            >
+              <g>
+                <circle cx="32" cy="32" r="31" fill="#008fde" />
+              </g>
+            </svg>
+            <HatenaLogo>B!</HatenaLogo>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+const HatenaLogo = styled.div`
+  top: 50%;
+  left: 50%;
+  color: white;
+  position: absolute;
+  transform: translateY(-50%) translateX(-50%);
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight: bold;
+`
 
 export default styled(SocialLinks)`
   display: flex;
@@ -41,5 +74,10 @@ export default styled(SocialLinks)`
 
   .share-count {
     text-align: center;
+  }
+
+  path {
+    transform: scale(0.9);
+    transform-origin: center;
   }
 `
