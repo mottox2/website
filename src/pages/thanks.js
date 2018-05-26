53f4d15d9da1326b919fa3ca98c5c94ff9849ee4
mottox2 <y.mottox2@gmail.com>
2018-05-26 14:20:06 +0900

    Move component

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Content from '../../components/Content';
-import { Wrapper } from '../../templates/PostTemplate';
+import styled from 'styled-components'
+import Content from '../components/Content';
+import { Wrapper } from '../templates/PostTemplate';
 
-const ContactPage = () => (
+const ContactThanksPage = () => (
   <Wrapper>
     <h1 style={{fontWeight: 600}}>お問い合わせ</h1>
     <Content>
       <p>
-        <b>お問い合わせありがとうございます</b><br/>
+        お問い合わせありがとうございます<br/>
         数日以内にお返事いたします。しばらくお待ち下さい。<br/>
         急ぎの用件でしたらTwitterのDMの方が早いので、そちらからも連絡してもらえれば反応します。
       </p>
@@ -15,4 +16,4 @@ const ContactPage = () => (
   </Wrapper>
 )
 
-export default ContactPage
+export default ContactThanksPage
