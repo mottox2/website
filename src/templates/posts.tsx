b6068a9be4d5cddf296d3442f15400ce4ea58573
mottox2 <y.mottox2@gmail.com>
2018-12-24 22:39:17 +0900

    Add sidebar

--- src/templates/posts.tsx
+++ src/templates/posts.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import PostCell from '../components/PostCell'
+import Profile from '../components/Profile'
 
 const IndexPage = ({ pageContext, location }: any) => {
   const { group, index, first, last, additionalContext } = pageContext
@@ -14,36 +15,87 @@ const IndexPage = ({ pageContext, location }: any) => {
 
   return (
     <Layout location={location}>
+      <Helmet title={`mottox2 blog`}>
+        <meta
+          name="description"
+          content={
+            'mottox2のエンジニア・デザインブログ。RailsとかReactとかTypeScriptとかを中心に書いています。'
+          }
+        />
+      </Helmet>
       <Container>
-        <Helmet title={`mottox2 blog`}>
-          <meta
-            name="description"
-            content={
-              'mottox2のエンジニア・デザインブログ。RailsとかReactとかTypeScriptとかを中心に書いています。'
-            }
-          />
-        </Helmet>
-        {(tag || category) && (
-          <Title>
-            {tag || category}
-            <small>に関する記事</small>
-          </Title>
-        )}
-        <Grid>
-          {group.map(({ node }: any) => <PostCell key={node.number || node.link} post={node} />)}
-        </Grid>
-        <Pagination>
-          {!first && <Link to={previousUrl}>{'< Previous'}</Link>}
-          {!last && (
-            <Link style={{ marginLeft: 'auto' }} to={nextUrl}>
-              {'Next >'}
-            </Link>
+        <div style={{ flex: 1 }}>
+          {(tag || category) && (
+            <Title>
+              {tag || category}
+              <small>に関する記事</small>
+            </Title>
           )}
-        </Pagination>
+          {/* <Grid> */}
+          {group.map(({ node }: any) => (
+            <PostCell key={node.number || node.link} post={node} />
+          ))}
+          {/* </Grid> */}
+          <Pagination>
+            {!first && <Link to={previousUrl}>{'< Previous'}</Link>}
+            {!last && (
+              <Link style={{ marginLeft: 'auto' }} to={nextUrl}>
+                {'Next >'}
+              </Link>
+            )}
+          </Pagination>
+        </div>
+        <Sidebar>
+          <SideSectionTitle>PROFILE</SideSectionTitle>
+          <SideSection>
+            <Profile />
+          </SideSection>
+          <SideSectionTitle>PICKUP TAGS</SideSectionTitle>
+          <SideSection>
+            <Tag to="/tags/gatsby">gatsby</Tag>
+            <Tag to="/tags/netlify">netlify</Tag>
+          </SideSection>
+        </Sidebar>
       </Container>
     </Layout>
   )
 }
+
+const Tag = styled(Link)`
+  display: inline-block;
+  background-color: #eee;
+  color: rgba(0, 0, 0, 0.6);
+  padding: 6px 12px;
+  margin-right: 8px;
+  font-weight: bold;
+  font-size: 12px;
+  border-radius: 4px;
+  text-decoration: none;
+  &:hover {
+    background-color: #ddd;
+  }
+  &:before {
+    content: "#";
+  }
+`
+
+const Sidebar = styled.aside`
+  min-width: 300px;
+  width: 300px;
+  margin-left: 40px;
+`
+
+const SideSection = styled.div`
+  margin: 12px 0 24px;
+`
+
+const SideSectionTitle = styled.h2`
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 8px;
+  letter-spacing: 1px;
+  color: #30627a;
+`
 
 const ScreenWidth = styled.div`
   @media (min-width: ${(320 + 24) * 2}px) {
@@ -56,7 +108,10 @@ const ScreenWidth = styled.div`
 `
 
 const Container = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
   margin-top: 12px;
+  display: flex;
   @media screen and (min-width: 600px) {
     margin-top: 24px;
   }
