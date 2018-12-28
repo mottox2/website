bef6e229580e7c8de7a296d4b60a9fd49c82ea5d
mottox2 <y.mottox2@gmail.com>
2018-12-29 01:32:34 +0900

    Add icon

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -92,6 +92,17 @@ export default class Search extends React.Component<Props, State> {
           onFocus={() => this.setState({ isActive: true })}
           onBlur={() => this.setState({ isActive: false })}
         />
+        <svg
+          css={icon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          style={{ fill: isActive ? '#555' : 'white' }}
+        >
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
         {isActive && query.length > 0 && (
           <>
             {filteredData.length > 0 ? (
@@ -150,10 +161,11 @@ const Base = styled.div`
 `
 
 const input = css`
-  padding: 8px 16px;
+  padding: 8px 16px 8px 40px;
   border-radius: 20px;
   background-color: rgba(255, 255, 255, 0.2);
   transition: background-color 0.15s;
+  outline: none;
   &:hover {
     background-color: rgba(255, 255, 255, 0.4);
   }
@@ -189,4 +201,13 @@ const listItem = css`
 const blankMessage = css`
   color: #888;
   padding: 12px;
+`
+
+const icon = css`
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-45%);
+  color: white;
+  fill: white;
 `
